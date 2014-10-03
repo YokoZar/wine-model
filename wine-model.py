10e548bc56ef1fc724c8d64490120b30bb5f8e07
Scott Ritchie <scottritchie@ubuntu.com>
2014-10-03 15:42:03 -0700

    Tweak default parameters a bit to get more visible variance

--- wine-model.py
+++ wine-model.py
@@ -32,25 +32,25 @@ CHART_TASKS_COMPLETE = False # This is often not helpful when comparing
 RANDOM_SEED = False # Set to a constant to directly compare strategies from one run to the next
 FINISH_TASKS_BEFORE_CHANGING_STRATEGY = True
 
-
-MIN_APPS_PER_USER = 1
-MAX_APPS_PER_USER = 10
-
 ###
-### Basic setup -- meant to be modified by user
+### Basic setup
 ###
 
 # Note that internally "features" == "apps" and "work items" == "bugs"
-number_of_bugs, number_of_apps, number_of_users = 10000, 2500, 5000
+number_of_bugs, number_of_apps, number_of_users = 25000, 2500, 5000
+
 # For additional options, see pick_methods below
 STRATEGIES_TO_TEST = ["Most popular feature", "Easiest feature", "Satisfy easiest user first", "Rotate strategies"]
+
+MIN_APPS_PER_USER = 1
+MAX_APPS_PER_USER = 5
 
 # TODO: document!
 def setup_functions():
     global bug_difficulty_function, bug_probability_function 
     global app_frequency_function, apps_per_user_function
     bug_difficulty_function = lambda: abs(int(random.normalvariate(4,3))) + 1
-    bug_probability_function = partial(probability_list_from_zipfs_law, number_of_bugs)
+    bug_probability_function = lambda: [x * .05 for x in probability_list_from_zipfs_law(number_of_bugs)]
     app_frequency_function = partial(frequency_list_from_pareto_distribution, number_of_apps)
     apps_per_user_function = partial(random.randint, MIN_APPS_PER_USER, MAX_APPS_PER_USER)
 
